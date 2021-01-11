ce277eddebd5ced28670bb3b039181c897bf467b
Fernando <fernando.bragga@gmail.com>
2021-01-11 23:14:11 +1100

    intial commit and landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * app created with yarn create react-app web --template typescript
+ */
+
+import React from "react";
+
+import "./assets/styles/global.css";
+import Landing from "./pages/Landing";
+
+function App() {
+  return <Landing />;
+}
+
+export default App;
